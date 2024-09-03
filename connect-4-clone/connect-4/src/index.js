857ef33d85e2ecb69abc0cdcf5170f8be4cf42aa
ATTA UR REHMAN <111627378+AttaUrRehman121@users.noreply.github.com>
2024-09-03 12:13:26 +0500

    Delete connect-4-clone/connect-4 directory

--- connect-4-clone/connect-4/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-import GameBoard from "./Components/GameBoard";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<GameBoard />);
